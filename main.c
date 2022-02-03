1750502cab63940c44b5a3efe77cef3f99dba2cb
JeanLuisJimenez <45690922+JeanLuisJimenez@users.noreply.github.com>
2022-02-02 20:05:52 -0400

    Adding comments and fixing a redundance on the  solve_mines function

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #define MAX_MINES 10
 
-void build_matrix(int rows, int cols, char (*matrix)[rows][cols]);
-void set_mines(int rows, int cols, char (*matrix)[rows][cols]);
-void print_table(int rows, int cols, char matrix[rows][cols], int show_solution);
-void solve_mines(int rows, int cols, char (*matrix)[rows][cols]);
+void build_table(int rows, int cols, char (*table)[rows][cols]);
+void set_mines(int rows, int cols, char (*table)[rows][cols]);
+void print_table(int rows, int cols, char table[rows][cols], int show_solution);
+void solve_mines(int rows, int cols, char (*table)[rows][cols]);
 void init_game();
 
 int main()
@@ -16,19 +16,22 @@ int main()
     return 0;
 }
 
-void build_matrix(int rows, int cols, char (*matrix)[rows][cols])
+void build_table(int rows, int cols, char (*table)[rows][cols])
 {
     int row, col;
     for (row = 0; row < rows; row++)
     {
         for (col = 0; col < cols; col++)
         {
-            (*matrix)[row][col] = '0';
+            /* Para crear el tablero, y no dejar los espacios vacios
+            decidi llenarlo con 0 los cuales quedaran igual si no se
+            detecta ninguna mina cerca */
+            (*table)[row][col] = '0';
         }
     }
 }
 
-void set_mines(int rows, int cols, char (*matrix)[rows][cols])
+void set_mines(int rows, int cols, char (*table)[rows][cols])
 {
     int row, col, currentMines = 0;
     time_t t;
@@ -38,15 +41,18 @@ void set_mines(int rows, int cols, char (*matrix)[rows][cols])
         row = rand() % rows;
         col = rand() % cols;
 
-        if ((*matrix)[row][col] != '*')
+        /* Para evitar que se posicionen 2 minas en el mismo
+        lugar primero se verifica que no exista ninguna mina
+        en es lugar para el momento. */
+        if ((*table)[row][col] != '*')
         {
-            (*matrix)[row][col] = '*';
+            (*table)[row][col] = '*';
             currentMines++;
         }
     }
 }
 
-void solve_mines(int rows, int cols, char (*matrix)[rows][cols])
+void solve_mines(int rows, int cols, char (*table)[rows][cols])
 {
     int row, col, mines;
 
@@ -55,47 +61,47 @@ void solve_mines(int rows, int cols, char (*matrix)[rows][cols])
         for (col = 0; col < cols; col++)
         {
             mines = 0;
-            if ((*matrix)[row][col] == '*') continue;
-            if (col - 1 >= 0 && row - 1 >= 0 && (*matrix)[row - 1][col - 1] == '*') mines++;
-            if (row - 1 >= 0 && (*matrix)[row - 1][col] == '*') mines++;
-            if (col + 1 < cols && row - 1 >= 0 && (*matrix)[row - 1][col + 1] == '*') mines++;
-            if (col - 1 >= 0 && (*matrix)[row][col - 1] == '*') mines++;
-            if (col + 1 < cols && (*matrix)[row][col + 1] == '*') mines++;
-            if (col - 1 >= 0 && row + 1 < rows && (*matrix)[row + 1][col - 1] == '*') mines++;
-            if (row + 1 < rows && (*matrix)[row + 1][col] == '*') mines++;
-            if (col + 1 < cols && row + 1 < rows && (*matrix)[row + 1][col + 1] == '*') mines++;
+            // Si es una mina no es necesario verificar minas de alrededor
+            if ((*table)[row][col] == '*') continue;
 
+            /* Para analizar cada posicion al rededor de la posicion actual
+            cada una de las siguientes lineas revisan una posicion empezando por
+            la esquina superior izquierda, y terminando en la esquina inferior derecha
+            de las posiciones al rededor. */
+            if (col - 1 >= 0 && row - 1 >= 0 && (*table)[row - 1][col - 1] == '*') mines++;
+            if (row - 1 >= 0 && (*table)[row - 1][col] == '*') mines++;
+            if (col + 1 < cols && row - 1 >= 0 && (*table)[row - 1][col + 1] == '*') mines++;
+            if (col - 1 >= 0 && (*table)[row][col - 1] == '*') mines++;
+            if (col + 1 < cols && (*table)[row][col + 1] == '*') mines++;
+            if (col - 1 >= 0 && row + 1 < rows && (*table)[row + 1][col - 1] == '*') mines++;
+            if (row + 1 < rows && (*table)[row + 1][col] == '*') mines++;
+            if (col + 1 < cols && row + 1 < rows && (*table)[row + 1][col + 1] == '*') mines++;
 
-            (*matrix)[row][col] = '0' + mines;
+            (*table)[row][col] += mines;
         }
 
     }
 }
 
-void print_table(int rows, int cols, char matrix[rows][cols], int show_solution)
+void print_table(int rows, int cols, char table[rows][cols], int show_solution)
 {
     int row, col;
-
-    if (show_solution)
-    {
-        printf("Matriz generada:\n");
-    }
-    else
-    {
-        printf("Soluci%cn del juego:\n", 162);
-    }
 
     for (row = 0; row < rows; row++)
     {
         for (col = 0; col < cols; col++)
         {
-            if (matrix[row][col] == '*')
+            if (table[row][col] == '*')
             {
                 printf("* ");
             }
+            /* Para no hacer 2 funciones con objetivos similares
+            añadi una condicion para decidir si mostrar la solucion o no,
+            de esta manera el codigo puede ser reutilizado para imprimir
+            tanto la solucion como el tablero solo con minas */
             else if (show_solution)
             {
-                printf("%c ", matrix[row][col]);
+                printf("%c ", table[row][col]);
             }
             else
             {
@@ -125,14 +131,19 @@ void init_game()
 
         scanf("%d %d", &rows, &cols);
 
-        char matrix[rows][cols];
+        char table[rows][cols];
 
-        build_matrix(rows, cols, &matrix);
+        build_table(rows, cols, &table);
 
-        set_mines(rows, cols, &matrix);
-        solve_mines(rows, cols, &matrix);
-        print_table(rows, cols, matrix, 0); // 0 means to no show table solution
-        print_table(rows, cols, matrix, 1); // 1 means to show table solution
+        set_mines(rows, cols, &table);
+
+        solve_mines(rows, cols, &table);
+
+        printf("Matriz generada:\n");
+        print_table(rows, cols, table, 0); // 0 no mostrar la solucion
+
+        printf("Soluci%cn del juego:\n", 162);
+        print_table(rows, cols, table, 1); // 1 mostrar la solucion
     }
     else
     {
